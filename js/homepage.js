309b071c9ad38a54dec24afd2acb36d4b896a614
clariojohan <clariojohan@gmail.com>
2021-04-22 17:33:08 +0700

    Homepage 98%

--- js/homepage.js
+++ js/homepage.js
@@ -13,3 +13,33 @@
     $("#post-content").show();
     $("#popup-post-content").hide();
   });
+
+// //   buat munculin login
+//   $(".login").click(()=>{
+//     $("#popup-login").show();
+//   });
+
+// //   buat munculin register
+//     $(".register").click(()=>{
+//       $("#popup-register").show();
+//     });
+
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
